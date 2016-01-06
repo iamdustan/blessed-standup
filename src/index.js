09245ec3ef2fe43d40eb4926b9ffcee4a8f20584
Dustan Kasten <dustan.kasten@gmail.com>
2016-01-06 16:08:29 -0500

    autoupdate message

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
+import updateNotifier from 'update-notifier';
+import chalk from 'chalk';
+
 import React from 'react';
-import screen from './screen';
+import {create} from './screen';
 import App from './app';
 import {render} from 'react-blessed';
+import {argv} from 'yargs'
 
-render(<App screen={screen} />, screen);
+import pkg from '../package.json';
+const notifier = updateNotifier({
+  pkg,
+  updateCheckInterval: 0,
+});
+
+if (!argv.force && notifier.update) {
+  const current = `(current: ${notifier.update.current})`;
+  console.log(`Update available: ${chalk.green.bold(notifier.update.latest)} ${chalk.gray(current)}`);
+  console.log(`Run ${chalk.magenta('npm install -g ' + pkg.name)} to update.`);
+  console.log(`Run ${chalk.magenta(pkg.name + ' --force')} to ignore update.`);
+} else {
+  const screen = create();
+  render(<App screen={screen} />, screen);
+}
 
--- src/screen.js
+++ src/screen.js
@@ -1,28 +1,30 @@
 import path from 'path';
 import blessed from 'blessed';
 
-const screen = blessed.screen({
-  autoPadding: true,
-  smartCSR: true,
-  title: 'blessed-standup',
-  debug: process.env.NODE_ENV !== 'production',
-  dockBorders: true,
-  // dump: path.join(__dirname, '..', 'log', 'process.log'),
-});
+export const create = () => {
+  const screen = blessed.screen({
+    autoPadding: true,
+    smartCSR: true,
+    title: 'blessed-standup',
+    debug: process.env.NODE_ENV !== 'production',
+    dockBorders: true,
+    // dump: path.join(__dirname, '..', 'log', 'process.log'),
+  });
 
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-  return process.exit(0);
-});
+  screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+    return process.exit(0);
+  });
 
-process.on('uncaughtException', function(err) {
-  if (/blessedstandup/.test(err.message)) {
-    console.error('ERROR! ' + err.message);
-    process.exit(1);
-  }
+  process.on('uncaughtException', function(err) {
+    if (/blessedstandup/.test(err.message)) {
+      console.error('ERROR! ' + err.message);
+      process.exit(1);
+    }
 
-  console.error('Caught exception: ' + err);
-  console.log(err.stack);
-});
+    console.error('Caught exception: ' + err);
+    console.log(err.stack);
+  });
 
-export default screen;
+  return screen;
+};
 
